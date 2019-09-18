6a8e68c62a41b4e2c4a7d71a493509dc9df4817e
seth@mondido <sethnejame>
2019-09-18 07:01:05 +0200

    Adding an instance of Auth to App.js, left some comments on how it works and also how we pass in auth to the home component form the router setup

--- src/App.js
+++ src/App.js
@@ -3,14 +3,31 @@ import { Route } from "react-router-dom";
 import Home from "./Home";
 import Profile from "./Profile";
 import Nav from "./Nav";
+import Auth from "./Auth/Auth";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    // hist will be auto injected into this component (props) because
+    // we wrapped "App" in Router in Index.js
+    // we pass the hist into Auth so Auth can interact with Router
+    this.auth = new Auth(this.props.history);
+  }
   render() {
     return (
       <>
         <Nav />
         <div className="body">
-          <Route path="/" exact component={Home} />
+          <Route
+            path="/"
+            exact
+            // below, we're declaring Home as a prop
+            // but we need to pass it auth as a prop
+            // for this, we use render and the below syntax
+            // render takes props as an arg, then we pass in
+            // Home w/ auth props, then spread any other props
+            render={props => <Home auth={this.auth} {...props} />}
+          />
           <Route path="/profile" component={Profile} />
         </div>
       </>
--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,7 +1,21 @@
 import auth0 from "auth0-js";
 
-export default class Auth  {
+export default class Auth {
+  // below we pass in the history from React Router
   constructor(history) {
-    
+    this.history = history;
+    this.auth0 = new auth0.WebAuth({
+      domain: process.env.REACT_APP_AUTH0_DOMAIN,
+      clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
+      redirectUri: process.env.REACT_APP_AUTH0_CALLBACK_URL,
+      responseType: "token id_token", // gives us both an id token (for authentication) and access token (for authorization)
+      scope: "openid profile email" // this means we wanna use openid for authentication. . .gives us access to user's profile (name, pic, etc)
+    });
+  }
+
+  login = () => {
+    // this is a method avail on the auth0.WebAuth object
+    // when authorize is called, it will redir browser to Auth0 login page
+    this.auth0.authorize();
   }
 }
