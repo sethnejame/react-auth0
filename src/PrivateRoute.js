f980ae6a63da8b2807d6cddd7ccbb198fd8fb0db
seth@mondido <sethnejame>
2019-09-19 15:20:46 +0200

    Adding a consumer to PrivateRoute

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,29 +1,34 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import PropTypes from "prop-types";
+import AuthContext from "./AuthContext";
 
-function PrivateRoute({ component: Component, auth, scopes, ...rest }) {
+function PrivateRoute({ component: Component, scopes, ...rest }) {
   return (
-    <Route
-      {...rest}
-      render={props => {
-        // 1. Redirect to login if not logged in.
-        if (!auth.isAuthenticated()) return auth.login();
+    <AuthContext.Consumer>
+      {auth => (
+        <Route
+          {...rest}
+          render={props => {
+            // 1. Redirect to login if not logged in.
+            if (!auth.isAuthenticated()) return auth.login();
 
-        // 2. Display message if user lacks required scope(s)
-        if (scopes.length > 0 && !auth.userHasScopes(scopes)) {
-          return (
-            <h1>
-              Unauthorized = You need the following scope(s) to view this page:{" "}
-              {scopes.join(",")}.
-            </h1>
-          );
-        }
+            // 2. Display message if user lacks required scope(s)
+            if (scopes.length > 0 && !auth.userHasScopes(scopes)) {
+              return (
+                <h1>
+                  Unauthorized = You need the following scope(s) to view this
+                  page: {scopes.join(",")}.
+                </h1>
+              );
+            }
 
-        // 3. Render component
-        return <Component auth={auth} {...props} />;
-      }}
-    />
+            // 3. Render component
+            return <Component auth={auth} {...props} />;
+          }}
+        />
+      )}
+    </AuthContext.Consumer>
   );
 }
 
