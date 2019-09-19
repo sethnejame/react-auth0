8e57a91c6f3bab05a0f1769aec5cc31b3678b613
seth@mondido <sethnejame>
2019-09-19 08:41:48 +0200

    Confguring Express to support reading a JWT and handle private API req

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
-// express is a Node-based library used for creating APIs
-const express = require("express");
-// dotenv automatically gives us access to environment variables (.env) inside this file
-require("dotenv").config();
+const express = require("express"); // express is a Node-based library used for creating APIs
+require("dotenv").config(); // dotenv automatically gives us access to environment variables (.env) inside this file
+const jwt = require("express-jwt"); // Validate JWT and set req.user
+const jwksRsa = require("jwks-rsa"); // Retrieve RSA keys from a JSON Web Key set (JWKS) endpoint
+
+const checkJwt = jwt({
+  // Dynamically provide a signing key based on the kid in the header
+  // and the signing keys provided by the JWKS endpoint
+  secret: jwksRsa.expressJwtSecret({
+    cache: true, //cache the signing key
+    rateLimit: true,
+    jwksRequestsPerMinute: 5, // prevent attackers from requesting more than 5 per min
+    jwksUri: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer
+  audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+  issuer: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/`,
+
+  // This must match the algorithm selected in the Auth0 dashboard under your app's advanced settings under the OAuth tab
+  algorithms: ["RS256"]
+});
 
 // instantiates express
 const app = express();
@@ -9,6 +27,13 @@ const app = express();
 app.get("/public", function(req, res) {
   res.json({
     message: "Hello from a public API!"
+  });
+});
+
+// below is a private API that checks for a jot (JWT) before allowing access to the url
+app.get("/private", checkJwt, function(req, res) {
+  res.json({
+    message: "Hello from a private API!"
   });
 });
 
